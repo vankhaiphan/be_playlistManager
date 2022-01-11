d048b1c77fa9a960b3ed7f8ba4c4fb9cb02496a0
nicolas-masselot <nicolasmasselot0@gmail.com>
2022-01-11 15:43:34 +0100

    correction du problème de CORS

--- server.js
+++ server.js
@@ -14,8 +14,24 @@ const app = express();
 app.use(bodyParser.json({ limit: "5mb" }));
 // app.get("/", (req, res) => res.send("Hello World"));
 
-const cors = require ('cors');
-app.use(cors({origin: 'http://127.0.0.1:4200', credentials: true}));
+const cors = require('cors');
+//app.use(cors({origin: 'http://127.0.0.1:4200', credentials: true}));
+//app.use(cors({origin: 'http://localhost:4200', credentials: true}));
+
+let whitelist = ['http://127.0.0.1:4200', 'http://localhost:4200']
+
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    if(!origin) return callback(null, true);
+    if(whitelist.indexOf(origin) === -1){
+      var message = 'The CORS policy for this origin doesn\'t allow access from the particular origin.';
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
+}));
 
 app.use("/", routes);
 
